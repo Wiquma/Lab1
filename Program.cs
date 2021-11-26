4dcc04ddb1c35c9803223507bf0c649aef48a892
Wiquma <94995405+Wiquma@users.noreply.github.com>
2021-11-26 12:48:33 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first number and press enter: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Enter second number and press enter: ");
+            int b = int.Parse(Console.ReadLine());
+            Console.Write("result=" + Sum(a, b));
+        }
+        public static int Sum(int a, int b)
+        {
+            return (a + b);
+        }
+
+    }
+    
+}
